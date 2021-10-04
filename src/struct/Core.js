fe9dff93a6fff25908be6c1d97417dfdeea18919
Andrew Kong <50548798+DMNight6@users.noreply.github.com>
2021-10-04 13:38:25 +0800

    Fix winston logging, add command handler

--- src/struct/Core.js
+++ src/struct/Core.js
@@ -1,7 +1,7 @@
 const { Client, Intents, Collection } = require('discord.js');
 const { readdirSync } = require("fs");
 const { resolve } = require('path');
-const { WinstonLogger } = require('./Logger');
+const { WinstonLogger }= require('./Logger');
 
 module.exports = class Core extends Client {
     constructor(bot_token) {
@@ -18,12 +18,16 @@ module.exports = class Core extends Client {
         const eventFiles = readdirSync(resolve(__dirname, '..', 'events', 'client')).filter(file => file.endsWith('js'));
         for (const file of eventFiles) {
             const event = require(resolve(__dirname, "..", "events", "client", file));
-            (event.once ? this.once(event.name, (...args) => event.run(...args)) : this.on(event.name, (...args) => event.run(...args)))
+            (event.once? this.once(event.name, (...args) => event.run(...args)) : this.on(event.name, (...args) => event.run(...args)))
         }
     }
 
     async importCommands() {
-
+        const comamndFiles = readdirSync(resolve(__dirname, '..', 'commands')).filter(file=>file.endsWith('js'))
+        for (const file of comamndFiles) {
+            const command = require(resolve(__dirname, '..', 'commands', file))
+            this.commands.set(command.name, command)
+        }
     }
 
     async connect() {
--- src/struct/Logger.js
+++ src/struct/Logger.js
@@ -1,8 +1,9 @@
-const { createLogger, format, transports} = require('winston');
-exports.WinstonLogger = createLogger({
+const {createLogger, format, transports} = require('winston');
+
+module.exports.WinstonLogger = createLogger({
     transports: [new transports.Console()],
     exitOnError: false,
-    format: format.printf((info) => {
+    format: format.printf(info => {
         const {level, message} = info;
         const now = new Date().toLocaleString();
         return `[${level}] : ${message} (${now})`
