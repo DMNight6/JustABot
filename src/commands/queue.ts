7c3461e492680dba2b843cc57c27779d9fb83ec9
Andrew Kong <50548798+DMNight6@users.noreply.github.com>
2021-10-13 09:52:15 +0800

    commenting, trying to figure out queue.ts issue

--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -1,4 +1,5 @@
-import Discord from 'discord.js'
+import Discord, { ButtonInteraction, Interaction, MessageEmbed } from 'discord.js'
+import { Player, Queue } from 'erela.js'
 import { ICommand } from '../interface'
 
 const QueueCommand: ICommand = {
@@ -9,7 +10,61 @@ const QueueCommand: ICommand = {
         if (!player) return message.channel.send(`This guild doesn't have any player (yet)`)
         if (player?.queue.length === 0 && !player.queue.current) return message.channel.send(`kekw, this guild doesn't have any song playing + queue is empty`)
 
+        const embed = GetEmbedInList(player.queue, 10);
+        let currentPage = 0;
+
+        const row = new Discord.MessageActionRow().addComponents(
+            new Discord.MessageButton()
+                .setCustomId('back-bt')
+                .setStyle('PRIMARY')
+                .setLabel('Previous Page'),
+            
+            new Discord.MessageButton()
+                .setCustomId('foward-bt')
+                .setStyle('SECONDARY')
+                .setLabel('Next Page')
+        );
+
+        const sendEmbed = await message.channel.send({embeds: [embed[currentPage].setFooter(`Page ${currentPage + 1} of ${embed.length}`)], components: [row]})
+        
+        const filter = (r: ButtonInteraction) => r.customId in ['foward-bt', 'back-bt'] && r.user.id === message.author.id
+        const collector = sendEmbed.createMessageComponentCollector({filter: filter}) // Create Button Interaction Collector. I'll assume Collector time field is 1 = 1 second. Docs instruction unclear.
+
+        collector.on('collect', async (r) => {
+            switch(r.customId) {
+                case 'foward-bt':
+                    if(currentPage < embed.length - 1) {
+                        currentPage++
+                        await sendEmbed.edit({embeds: [embed[currentPage].setFooter(`Page ${currentPage + 1} of ${embed.length}`)], components: [row]});
+                    }
+                    break;
+                case 'back-bt':
+                    if (currentPage !== 0) {
+                        --currentPage;
+                        await sendEmbed.edit({embeds: [embed[currentPage].setFooter(`Page ${currentPage + 1} of ${embed.length}`)], components: [row]});
+                    }
+                    break;
+            }
+        })
+
+        collector.on('end', async () => {
+            await sendEmbed.delete()
+        })
     }
+}
+
+function GetEmbedInList(queue: Queue, pagePerItem: number) {
+    const embed = [];
+    for (let item = 0; item < queue.length; item += 10) {
+        const current = queue.slice(item, pagePerItem);
+        let count = item;
+        pagePerItem += pagePerItem;
+        const info = current.map((track) => `${++count} • [${track.title}](${track.uri})\n${Discord.Formatters.italic(`Requested By: <@${track.requester}>`)}`).join('\n');
+        const e = new Discord.MessageEmbed()
+            .setDescription(`[Now Playing • ${queue.current?.title}](${queue.current?.uri})\n\n${info}`)
+        embed.push(e);
+    }
+    return <Array<Discord.MessageEmbed>>embed
 }
 
 export default QueueCommand
--- src/events/client/raw.ts
+++ src/events/client/raw.ts
@@ -1,7 +1,7 @@
 import { IEvent } from "../../interface";
 
 const SendPayloadToLavalinkEvent: IEvent = {
-    // @ts-ignore
+    // @ts-ignore // Have to ignore this, as this event is pretty much a private event
     name: 'raw',
     once: false,
     run: async(client, d) => {
