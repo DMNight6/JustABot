25f32520cc21fd694cc06b951f4879280aaecc06
Andrew Kong <50548798+DMNight6@users.noreply.github.com>
2021-10-14 13:58:06 +0800

    docs, cleanup, I don't know whats wrong

--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -1,4 +1,4 @@
-import Discord, { ButtonInteraction, MessageReaction } from 'discord.js'
+import Discord from 'discord.js'
 import { Queue } from 'erela.js'
 import { ICommand } from '../interface'
 
@@ -14,46 +14,57 @@ const QueueCommand: ICommand = {
         if (!message.guild?.me?.permissionsIn(message.channel.id).has('MANAGE_MESSAGES')) return message.channel.send(`I refuse to show queue. I can't change the message if I miss the perms MANAGE_MESSAGE`)
         
         let currentPage = 0; // Inital page
-        const embed = GetEmbedInList(player.queue, 10);
+        const embed = await GetEmbedInList(player.queue, 5);
 
         const sendEmbed = await message.channel.send({embeds: [embed[currentPage].setFooter(`Page ${currentPage + 1} of ${embed.length}`)]});
         
         await sendEmbed.react('⬅') /* I don't really recommend to do this. Slow afaik but I have no choice */ 
         await sendEmbed.react('➡')
         
+        /* 
+            I wish I can use buttons but that would require interactions and keeps on returining errors. 
+            The problem is caused by interaction events is not called. (This would be fixed when Slash command support is added)
+        */
         const filter = (bt: Discord.MessageReaction, user: Discord.User) => ['⬅', '➡'].includes(bt.emoji.name!) && user.id === message.author.id;
         const collector = sendEmbed.createReactionCollector({filter: filter, time: 30_000}) // Apparently, 1_000 = 1 second in this lib which would make 1 = 1ms.
-            .on('collect', (bt) => {
+            .on('collect', async (bt) => {
                 switch (bt.emoji.name) {
                     case '⬅':
                         if (currentPage !== 0) {
-                            --currentPage
-                            sendEmbed.edit({embeds: [embed[currentPage].setFooter(`Page ${currentPage + 1} of ${embed.length}`)]})
+                            currentPage--;
+                            await sendEmbed.edit({embeds: [embed[currentPage].setFooter(`Page ${currentPage + 1} of ${embed.length}`)]})
                         }
                         break;
                     case '➡':
                         if (currentPage < embed.length) {
-                            ++currentPage
-                            sendEmbed.edit({embeds: [embed[currentPage].setFooter(`Page ${currentPage + 1} of ${embed.length}`)]})
+                            currentPage++;
+                            await sendEmbed.edit({embeds: [embed[currentPage].setFooter(`Page ${currentPage + 1} of ${embed.length}`)]})
                         }
                         break;
                 }
             })
+            .on('end', async () => {
+                await sendEmbed.delete()
+            })
     }
 }
 
-function GetEmbedInList(queue: Queue, pagePerItem: number) {
-    const embed = [];
-    for (let item = 0; item < queue.length; item += 10) {
-        const current = queue.slice(item, pagePerItem);
+async function GetEmbedInList(queue: Queue, pagePerItem: number): Promise<Discord.MessageEmbed[]> {
+    const embed = []; /* Array of embeds */ 
+    const valueRemainStatic = pagePerItem; /* Slight tweaks. This value won't change (hopefully) */
+    /* Pagination 2.0 kek */
+    for(let item = 0; item < queue.length; item += valueRemainStatic) {
+        const current = queue.slice(item, pagePerItem)
         let count = item;
-        pagePerItem += pagePerItem;
-        const info = current.map((track) => `${++count} • [${track.title}](${track.uri})\n${Discord.Formatters.italic(`Requested By: <@${track.requester}>`)}`).join('\n');
-        const e = new Discord.MessageEmbed()
-            .setDescription(`[Now Playing • ${queue.current?.title}](${queue.current?.uri})\n\n${info}`)
-        embed.push(e);
+        pagePerItem += pagePerItem
+
+        let info = current.map(async(track) => `[${++count} • ${track.title}](${track.uri})`).join('\n');
+        const embeds = new Discord.MessageEmbed()
+            .setDescription(`[Now Playing • ${queue.current?.title}](${queue.current?.uri})\n\n${info}`);
+
+        embed.push(embeds);
     }
-    return <Array<Discord.MessageEmbed>>embed
+    return embed;
 }
 
 export default QueueCommand
