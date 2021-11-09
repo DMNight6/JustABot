c16a717c6839ef04fdbe90c02294ccd141aceb89
Andrew Kong <50548798+DMNight6@users.noreply.github.com>
2021-11-09 18:53:38 +0800

    Fix cmd
    Fix seek command

--- src/commands/seek.ts
+++ src/commands/seek.ts
@@ -5,10 +5,9 @@ const SeekCommand: ICommand = {
     name: 'seek',
     desc: 'Seek the position of desired time of track.',
     category: "Music",
-    usage: '[Time (1m1s/1m/1s/1)]',
+    usage: '[Time (1m1s/1m/1s)]',
     run: async (client, message, args) => {
         let match = args.toLocaleString().match(stringMatch); // Match string with RegExp. ^RegExp. #L3 (RegExp)^
-
         let ovableTime: number = -1; // Overwriteable. default to -1 for a reason. (^#L36 explains why.^)
         if (!match) return message.channel.send('Invalid Time String');
         let filterMatch: Array<string> = await sortRegArray(match);
@@ -16,12 +15,18 @@ const SeekCommand: ICommand = {
         /* Rewrite Time Sorting */
         switch (filterMatch.length) {
             case 4:
-                let mstime = +filterMatch[1] * 60 * 1000 + +filterMatch[3] * 1000
-                ovableTime = mstime;
+                let msTime = +filterMatch[1] * 60 * 1000 + (+filterMatch[3] * 1000) // Preventing confusion.
+                ovableTime = msTime
                 break;
             case 2:
-                let stime = +filterMatch[1] * 1000
-                ovableTime = stime;
+                if (filterMatch[0].endsWith('m')) {
+                    let mTime = +filterMatch[1] * 60 * 1000
+                    ovableTime = mTime
+                }
+                else {
+                    let sTime = +filterMatch[1] * 1000
+                    ovableTime = sTime
+                }
                 break;
         }    
         /* End of Time Rewrite */
@@ -29,7 +34,7 @@ const SeekCommand: ICommand = {
         const player = client.Music.get(message.guild?.id!)
         if (!player) return message.channel.send('This guild doesn\'t have any player (yet)')
 
-        if (player.queue.current?.duration! > ovableTime && ovableTime > -1 && player.queue.current?.isStream) { // When ovableTime is less or equal than -1 || ovableTime is > than current track duration || current track is stream, this function is skipped and moved to else part.
+        if (player.queue.current?.duration! > ovableTime && ovableTime > -1 && !player.queue.current?.isStream) { // When ovableTime is less or equal than -1 || ovableTime is > than current track duration || current track is stream, this function is skipped and moved to else part.
             player.seek(ovableTime)
             message.channel.send(`Seeked to ${args.toString()}`)
         } else return message.channel.send(`Invalid time range!`)
