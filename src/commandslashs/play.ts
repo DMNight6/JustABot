1bcabd9b853dbcb0fbbc4586e82ef3089cad5cda
Andrew Kong <50548798+DMNight6@users.noreply.github.com>
2021-12-09 08:29:04 +0800

    Fix interaction for play command

--- src/commandslashs/play.ts
+++ src/commandslashs/play.ts
@@ -52,7 +52,7 @@ const PlaySlashCommand: SlashCommandBuilder = {
             player.queue.add(result.tracks[0])
             if (!shouldPlayNow) interaction.reply({embeds: [new MessageEmbed().setAuthor(`Added To Queue`, interaction.user.displayAvatarURL()!).setDescription(`[${result.tracks[0].title}](${result.tracks[0].uri})\nArtist • ${result.tracks[0].author}`).setColor('RANDOM').setFooter(`Requested by • ${interaction.member.user}`, interaction.user.displayAvatarURL()!).setColor('RANDOM').setThumbnail(result.tracks[0].displayThumbnail('maxresdefault'))], ephemeral: true})
             else {
-                interaction.reply({content: 'There is no song in queue, so I will just play the music.', ephemeral: true})
+                await interaction.reply({content: 'There is no song in queue, so I will just play the music.', ephemeral: true})
                 await player.play();
             }
         } else if (result.loadType === `PLAYLIST_LOADED`) {
@@ -62,9 +62,9 @@ const PlaySlashCommand: SlashCommandBuilder = {
             if (!shouldPlayNow) await interaction.reply({embeds: [new MessageEmbed().setAuthor(`Added To Queue`, interaction.user.displayAvatarURL()!).setDescription(`[${result.playlist.name}](${result.playlist.selectedTrack?.uri})\nTracks • ${result.tracks.length}`).setFooter(`Requested By • ${interaction.member.user}`, interaction.user.displayAvatarURL()!).setColor('RANDOM').setThumbnail(result.playlist.selectedTrack?.displayThumbnail('maxresdefault')!)], ephemeral: true})
             else await player.play();
         } else if (result.loadType === `NO_MATCHES`) {
-            interaction.reply({ content: `My bad, Seems like I can't find the songs you queried for`})
+            await interaction.reply({ content: `My bad, Seems like I can't find the songs you queried for`})
         } else if (result.loadType === 'LOAD_FAILED') {
-            interaction.reply({ content: `keeeeeeeek, an error occured whiille loading the traaaacks.`})
+            await interaction.reply({ content: `keeeeeeeek, an error occured whiille loading the traaaacks.`})
         }
     }
 }
