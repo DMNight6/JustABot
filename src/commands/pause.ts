dff88fed2d8ed140c749ccaf970b7f4bc1ef4578
DMNight6 <50548798+DMNight6@users.noreply.github.com>
2022-02-17 13:42:57 +0800

    Forgotten codes

--- src/commands/pause.ts
+++ src/commands/pause.ts
@@ -21,24 +21,24 @@ const PauseCommand: ICommand = {
             let time: number = 0;
 
             let FilteredSplit = string.split(expression).filter(n => n);
-            
+
             if (FilteredSplit.length == 4 && FilteredSplit[1] == 'n' && FilteredSplit[3] == 's' && !isNaN(+FilteredSplit[0]) && !isNaN(+FilteredSplit[3])) {
                 time = (+FilteredSplit[0] * 60 * 1_000) + (+FilteredSplit[3] * 1_000);
             } else if (FilteredSplit.length == 2 && FilteredSplit[1] == 'm' && !isNaN(+FilteredSplit[0])) {
                 time = +FilteredSplit[0] * 60 * 1_000;
             } else if (FilteredSplit.length == 2 && FilteredSplit[1] == 's' && !isNaN(+FilteredSplit[0])) {
                 time = +FilteredSplit[0] * 1_000;
-            } else return interaction.reply({ content: 'Invalid time Format!, `Formats • 1m10s, 10s, 10m`' });
+            } else return interaction.reply({ content: 'Invalid time Format!, `Formats • 1m10s, 10s, 10m`', ephemeral: true });
 
             if (!client.TimerCFG[interaction.guild?.id!]) {
                 player.pause(true);
-                client.TimerCFG[interaction.guild?.id!] = setTimeout(() => { interaction.reply({ content: 'Unpaused player due to requested time reached.' }); player.pause(false) }, time);
+                client.TimerCFG[interaction.guild?.id!] = setTimeout(() => { interaction.editReply({ content: 'Unpaused player due to requested time reached.' }); player.pause(false) }, time);
             }
 
-            return interaction.reply({ content: 'Paused the player for ' + ConvertTime(time)});
+            return interaction.reply({ content: 'Paused the player for ' + ConvertTime(time), ephemeral: true});
         } else if (!player.paused) {
             player.pause(true);
-            return interaction.reply({ content: 'Player paused without time limit. '});
+            return interaction.reply({ content: 'Player paused without time limit. ', ephemeral: true });
         };
 
         if (player.paused) {
@@ -47,9 +47,9 @@ const PauseCommand: ICommand = {
                 clearTimeout(client.TimerCFG[interaction.guild?.id!]);
                 delete client.TimerCFG[interaction.guild?.id!];
                 // Cleanup.
-                return interaction.reply({ content: 'Unpaused the player and canceled the unpause timer.' });
+                return interaction.reply({ content: 'Unpaused the player and canceled the unpause timer.', ephemeral: true });
             };
-            return interaction.reply({ content: 'Unpaused the player.' });
+            return interaction.reply({ content: 'Unpaused the player.', ephemeral: true });
         };
     }
 };
